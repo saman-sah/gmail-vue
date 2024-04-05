905b14981cbf3119455af8663ee8aad4f73bad1a
SamanSah <sahraei.saman91@gmail.com>
2024-04-05 04:46:59 +0330

    init email routing and create icon template

---
+++ src/components/IconComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div 
+    id="IconComponent"
+  >
+    <div :class="[
+      hoverColor,
+      'flex items-center justify-center rounded-full cursor-pointer w-10 h-10'
+      ]"
+      :data-tooltip-target="`tooltip-no-arrow-${iconName}`"
+      data-tooltip-placement="bottom"
+    >
+      <component 
+        :is="icon"
+        :size="iconSize" 
+        :fillColor="iconColor"
+      />
+    </div>
+    <div 
+      :id="`tooltip-no-arrow-${iconName}`"
+      role="tooltip"
+      class="inline-block absolute invisible text-xs z-10 py-1 bg-gray-500 px-2 font-medium text-white rounded-sm shadow-sm opacity-0 tooltip darK:bg-gray-600 delay-150"
+    >
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  iconColor: {
+    type: String,
+    default: ''
+  },
+  iconSize: {
+    type: String,
+    default: ''
+  },
+  icon: {
+    type: String,
+    default: ''
+  },
+  text: {
+    type: String,
+    default: ''
+  },
+  hoverColor: {
+    type: String,
+    default: ''
+  },
+  iconName: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
---
+++ src/views/template/EmailView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div 
+    id="EmailView" 
+    class="bg-gray-50 h-screen"
+  >
+    <div class="flex justify-start items-center px-3.5">
+      <IconComponent 
+        :icon="MenuIcon"
+        icon-size="19"
+        icon-color="#636363"
+        text="dsfsdfsd"
+        hover-color="hover:bg-gray-200"
+        class="-ml-2 mr-2"
+        icon-name="menu"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IconComponent from '@/components/IconComponent.vue';
+import MenuIcon from 'vue-material-design-icons/Menu.vue'
+</script>
